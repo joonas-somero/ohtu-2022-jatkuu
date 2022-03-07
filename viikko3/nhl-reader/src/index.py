f68bf7fc12dd38ae846fa2934b8261f5249c9139
Joonas Somero <joonas.somero@protonmail.com>
2022-03-07 10:40:54 +0200

    Viikko 3, tehtävä 1

---
+++ viikko3/nhl-reader/src/index.py
@@ -0,0 +1,36 @@
+import requests
+from datetime import datetime
+from player import Player
+
+def main():
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+#    print("JSON-muotoinen vastaus:")
+#    print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name'],
+            player_dict['nationality'],
+            player_dict['assists'],
+            player_dict['goals'],
+            player_dict['penalties'],
+            player_dict['team'],
+            player_dict['games']
+        )
+
+        players.append(player)
+
+    nationality = "FIN"
+    print(f"Players from {nationality} {datetime.now()}\n")
+
+    for player in players:
+        if player.nationality == nationality:
+            print(player)
+
+
+if __name__ == "__main__":
+    main()
