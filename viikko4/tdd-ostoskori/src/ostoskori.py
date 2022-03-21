4421de837fe5fc2b205c5c3ce12423d83bb4be80
Joonas Somero <joonas.somero@protonmail.com>
2022-03-21 13:59:05 +0200

    Viikko 4, tehtävä 5: kahden saman ostoksen määrä

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -4,21 +4,25 @@ from ostos import Ostos
 class Ostoskori:
     def __init__(self):
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
-        self._kori = []
+        self._kori = {}
 
     def tavaroita_korissa(self):
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
-        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
-        return len(self._kori)
+        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2
+        return sum(ostos.lukumaara() for ostos in self._kori.values())
 
     def hinta(self):
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
-        return sum(ostos.hinta() for ostos in self._kori)
+        return sum(ostos.hinta() for ostos in self._kori.values())
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        self._kori.append(Ostos(lisattava))
+        nimi = lisattava.nimi()
+        if nimi in self._kori:
+            self._kori[nimi].muuta_lukumaaraa(1)
+        else:
+            self._kori[nimi] = Ostos(lisattava)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -31,4 +35,4 @@ class Ostoskori:
     def ostokset(self):
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
-        return self._kori
+        return list(self._kori.values())
